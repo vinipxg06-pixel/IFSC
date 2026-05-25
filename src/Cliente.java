3709feca520ec52ea242de26742993e4f658cafc
vinipxg06-pixel <vinipxg06@gmail.com>
2026-05-25 16:47:38 -0300

    Iniciando Classe Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente {
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+
+    public Cliente(int id, String telefone, String email, String cpf, String nome) {
+        this.id = id;
+        this.telefone = telefone;
+        this.email = email;
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+
+
+}
